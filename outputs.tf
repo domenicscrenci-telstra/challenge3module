63cc5984b8d292f3e053bbe88759f2516563b789
Domenic Screnci <domenic.screnci@team.telstra.com>
2022-03-02 22:09:30 +1100

    changed outputs to match expected

--- outputs.tf
+++ outputs.tf
@@ -1,34 +1,14 @@
-output "vpc_id" {
-  description = "The ID of the VPC"
-  value       = aws_vpc.hashicat.id
+output "private_key_pem" {
+  description = "The private key data in PEM format"
+  value = tls_private_key.hashicat.private_key_pem
 }
 
-output "subnet_id" {
-  description = "The ID of the subnet"
-  value       = aws_subnet.hashicat.id
+output "public_ip" {
+  description = "Public IP address of Elastic IP"
+  value = aws_eip.hashicat.public_ip
 }
 
-output "securitygroup_id" {
-  description = "The ID of the security group"
-  value       = aws_security_group.hashicat.id
-}
-
-output "internetgateway_id" {
-  description = "The ID of the SG"
-  value       = aws_internet_gateway.hashicat.id
-}
-
-output "routetable_id" {
-  description = "The ID of the SG"
-  value       = aws_route_table.hashicat.id
-}
-
-output "eip_id" {
-  description = "The ID of the EIP"
-  value       = aws_eip.hashicat.id
-}
-
-output "ec2_id" {
-  description = "The ID of the EC2 instance"
-  value       = aws_instance.hashicat.id
+output "public_dns" {
+  description = "Public DNS associated with the Elastic IP address"
+  value = aws_eip.hashicat.public_dns
 }
